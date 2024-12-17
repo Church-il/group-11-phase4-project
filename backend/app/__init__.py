141735d65eacd47f50471663a373dec82c34baae
Michael Akoko <akokomichael37@gmail.com>
2024-12-17 17:17:22 +0300

    insert changes

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
@@ -24,12 +24,13 @@ def create_app():
     bcrypt.init_app(app)
     CORS(app)  
     
-    #Blueprints
+    # Blueprints
     from app.routes.auth_routes import auth_bp
     from app.routes.user_routes import user_bp
     from app.routes.project_routes import project_bp
     from app.routes.membership_routes import membership_bp
 
+    # Register blueprints
     app.register_blueprint(auth_bp, url_prefix='/api/auth')
     app.register_blueprint(user_bp, url_prefix='/api/users')
     app.register_blueprint(project_bp, url_prefix='/api/projects')
